a4c65c76e2feab9b220191f11f7e4578a9041b43
Ky Nguyen <kynguyen@KNs-MBP.local>
2017-06-08 03:33:19 +0700

    Reformat signup
    - Change directory storage, change file name

---
+++ MyEvents/Features/Signup/Signup.swift
@@ -0,0 +1,210 @@
+//
+//  Signup.swift
+//  MyEvents
+//
+//  Created by Ky Nguyen on 6/8/17.
+//  Copyright © 2017 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class meSignupController: knTableController {
+    
+    var output: meSignupControllerOutput?
+    
+    fileprivate let backgroundImageView: UIImageView = {
+        
+        let imageName = "membership_background"
+        let iv = UIImageView(image: UIImage(named: imageName))
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    internal lazy var emailTextField: UITextField = { [weak self] in
+        
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.font = UIFont.systemFont(ofSize: 17)
+        tf.textColor = .black
+        tf.placeholder = "Email"
+        tf.autocorrectionType = .no
+        tf.autocapitalizationType = .none
+        tf.delegate = self
+        tf.keyboardType = .emailAddress
+        
+        let underline = UIView()
+        underline.tag = 101
+        underline.translatesAutoresizingMaskIntoConstraints = false
+        underline.backgroundColor = UIColor.color(value: 141)
+        
+        tf.addSubview(underline)
+        underline.horizontal(toView: tf)
+        underline.bottom(toView: tf)
+        underline.height(0.5)
+        
+        return tf
+        }()
+    
+    internal lazy var passwordTextField: UITextField = { [weak self] in
+        
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.font = UIFont.systemFont(ofSize: 17)
+        tf.textColor = .black
+        tf.isSecureTextEntry = true
+        tf.placeholder = "Password"
+        tf.delegate = self
+        
+        let underline = UIView()
+        underline.tag = 101
+        underline.translatesAutoresizingMaskIntoConstraints = false
+        underline.backgroundColor = UIColor.color(value: 141)
+        
+        tf.addSubview(underline)
+        underline.horizontal(toView: tf)
+        underline.bottom(toView: tf)
+        underline.height(0.5)
+        return tf
+        }()
+    
+    internal lazy var signupButton: UIButton = { [weak self] in
+        let button = UIButton()
+        button.isEnabled = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Next", for: .normal)
+        button.setTitleColor(UIColor.color(r: 141, g: 141, b: 141, alpha: 0.5), for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        button.addTarget(self, action: #selector(handleSignup), for: .touchUpInside)
+        return button
+        }()
+    
+    func handleSignup() {
+        print("Signup tapped")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        meSignupConfiguration.shared.configure(viewController: self)
+        setupView()
+    }
+    
+    override func setupView() {
+        
+        navigationController?.isNavigationBarHidden = true
+        
+        let gradientViewHeight: CGFloat = screenHeight
+        let gradientView = UIView()
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        gradientView.setupGradientLayer(colors: [UIColor.clear, UIColor.white],
+                                        size: CGSize(width: screenWidth, height: gradientViewHeight),
+                                        startPoint: CGPoint(x: 0.5, y: 0),
+                                        endPoint: CGPoint(x: 0.5, y: 1))
+        
+        let headerView = UIView()
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        headerView.addSubview(backgroundImageView)
+        headerView.addSubview(gradientView)
+        headerView.addSubview(emailTextField)
+        headerView.addSubview(passwordTextField)
+        headerView.addSubview(signupButton)
+        
+        gradientView.horizontal(toView: headerView)
+        gradientView.height(gradientViewHeight)
+        gradientView.bottom(toView: backgroundImageView)
+        
+        backgroundImageView.horizontal(toView: headerView)
+        backgroundImageView.top(toView: headerView, space: DeviceType.IS_IPHONE_5 ? -32 : 0)
+        backgroundImageView.square()
+        
+        passwordTextField.horizontal(toView: headerView, space: 16)
+        passwordTextField.bottom(toView: backgroundImageView, space: DeviceType.IS_IPHONE_5 ? -16 : 0)
+        passwordTextField.height(36)
+        
+        emailTextField.horizontal(toView: passwordTextField)
+        passwordTextField.verticalSpacing(toView: emailTextField, space: 24)
+        emailTextField.height(toView: passwordTextField)
+        
+        signupButton.right(toView: headerView, space: -16)
+        signupButton.verticalSpacing(toView: passwordTextField, space: 24)
+        
+        headerView.height(screenHeight)
+        
+        tableView.isScrollEnabled = false
+        
+        tableView.tableHeaderView = UIView()
+        tableView.tableHeaderView?.frame.size.height = screenHeight
+        tableView.tableHeaderView?.addSubview(headerView)
+        tableView.tableHeaderView?.addConstraints(withFormat: "H:|[v0]|", views: headerView)
+        tableView.tableHeaderView?.addConstraints(withFormat: "V:|[v0]", views: headerView)
+        
+        emailTextField.becomeFirstResponder()
+    }
+    
+    override func fetchData() {
+        
+    }
+    
+}
+
+
+
+extension meSignupController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let newText = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        
+        NSObject.cancelPreviousPerformRequests(withTarget: self)
+        perform(#selector(prepareToValidate), with: (textField, newText), afterDelay: 0.5)
+        
+        textField.text = newText
+        return false
+    }
+    
+    func prepareToValidate(data: Any) {
+        guard let (textField, text) = data as? (UITextField, String) else { return }
+        
+        var email = ""
+        var password = ""
+        
+        if textField == emailTextField {
+            email = text
+            password = passwordTextField.text!
+        }
+        else {
+            email = emailTextField.text!
+            password = text
+        }
+        
+        output?.validate(email: email, password: password)
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MyEvents/Features/Signup/SignupVIP.swift
@@ -0,0 +1,149 @@
+//
+//  SignupVIP.swift
+//  MyEvents
+//
+//  Created by Ky Nguyen on 6/8/17.
+//  Copyright © 2017 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Implementation
+ 1. Replace 'meSignup' to your controller's name
+ 2. Cut 'var output : meSignupControllerOutput? /* output */' to your real controller file -> Delete class 'meSignupController'
+ 3. Define actions controller needs in protocol meSignupControllerOutput
+ 4. Define actions to respond to result in protocol 'meSignupInteractorOutput'. For instance: requestSuccess, updateView...
+ 5. Conform actions from 'meSignupControllerOutput' in 'extension meSignupInteractor'
+ 6. Define actions to update directly to UI to 'meSignupPresenterOutput'
+ 7. Conform actions from 'meSignupInteractorOutput'
+ 8. Conform actions and update UI
+ */
+
+
+
+// 3
+protocol meSignupControllerOutput: class {
+    
+    func validate(email: String, password: String)
+}
+
+// 4
+protocol meSignupInteractorOutput: class {
+    
+    func finishedValidation(error: knError?)
+}
+
+// 5
+
+extension meSignupInteractor: meSignupControllerOutput {
+    
+    func validate(email: String, password: String) {
+        meValidateEmailPasswordWorker(email: email, password: password,
+                                      completion: output?.finishedValidation).execute()
+    }
+}
+
+// 6 - Define protocol to update directly to UI
+protocol meSignupPresenterOutput: class {
+    func displayValidationResult(_ error: knError?)
+}
+
+
+// 7: Convert Model to View Model to update UI
+extension meSignupPresenter: meSignupInteractorOutput {
+    
+    func finishedValidation(error: knError?) {
+        output?.displayValidationResult(error)
+    }
+}
+
+
+// 8 - Update UI
+extension meSignupController: meSignupPresenterOutput {
+    
+    func displayValidationResult(_ error: knError?) {
+        
+        let isSuccess = error == nil
+        
+        func responseToFailValidation(_ error: knError) {
+            signupButton.isEnabled = false
+            signupButton.setTitleColor(UIColor.color(r: 141, g: 141, b: 141, alpha: 0.5), for: .normal)
+            
+            var errorTextField: UITextField!
+            
+            switch error.code! {
+            case .emptyEmail, .invalidEmail:
+                errorTextField = emailTextField
+                
+            case .emptyPassword, .invalidPassword:
+                errorTextField = passwordTextField
+                
+            default:
+                break
+            }
+            
+            errorTextField.viewWithTag(101)?.backgroundColor = UIColor.color(r: 255, g: 56, b: 36)
+        }
+        
+        func responseToSuccessValidation() {
+            
+            let color141 = UIColor.color(value: 141)
+            
+            signupButton.isEnabled = true
+            signupButton.setTitleColor(color141, for: .normal)
+            
+            emailTextField.viewWithTag(101)?.backgroundColor = color141
+            passwordTextField.viewWithTag(101)?.backgroundColor = color141
+            
+        }
+        
+        if isSuccess == true {
+            responseToSuccessValidation()
+        }
+        else {
+            responseToFailValidation(error!)
+        }
+    }
+    
+    
+}
+
+
+
+
+
+class meSignupInteractor {
+    
+    var output: meSignupInteractorOutput?
+}
+
+class meSignupPresenter {
+    
+    weak var output: meSignupPresenterOutput?
+}
+
+//meK: Configuration
+class meSignupConfiguration /* called in viewDidLoad */ {
+    
+    static let shared = meSignupConfiguration()
+    
+    func configure(viewController: meSignupController) {
+        
+        // Presenter
+        let presenter = meSignupPresenter()
+        presenter.output = viewController
+        
+        // Interactor
+        let interactor = meSignupInteractor()
+        interactor.output = presenter
+        
+        // View controller
+        viewController.output = interactor
+    }
+    
+}
+
+
+
+
